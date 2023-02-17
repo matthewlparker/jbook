f170c71c8df5454e13bb4ddb0b93009d278a2bb4
Matthew <cadburylion@outlook.com>
2023-02-17 12:02:01 -0800

    CLI accepts user commands, starts local-api server on default or chosen port, and prints useful error log

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -8,8 +8,9 @@ const express_1 = __importDefault(require("express"));
 const serve = (port, filename, dir) => {
     // Initial setup for express server
     const app = (0, express_1.default)();
-    app.listen(port, () => {
-        console.log('Listening on port ', port);
+    // Asyncify server startup
+    return new Promise((resolve, reject) => {
+        app.listen(port, resolve).on('error', reject);
     });
 };
 exports.serve = serve;
--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -4,7 +4,8 @@ export const serve = (port: number, filename: string, dir: string) => {
   // Initial setup for express server
   const app = express();
 
-  app.listen(port, () => {
-    console.log('Listening on port ', port);
+  // Asyncify server startup
+  return new Promise<void>((resolve, reject) => {
+    app.listen(port, resolve).on('error', reject);
   });
 };
