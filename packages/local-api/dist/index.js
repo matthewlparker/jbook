890400f5d2f24bd8982a4062e63bee00a08e8345
Matthew <cadburylion@outlook.com>
2023-02-17 12:33:05 -0800

    local-api serves local-client through proxy at a different port

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -5,9 +5,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
 const express_1 = __importDefault(require("express"));
+const http_proxy_middleware_1 = require("http-proxy-middleware");
 const serve = (port, filename, dir) => {
     // Initial setup for express server
     const app = (0, express_1.default)();
+    // 1. Load up react application inside browser
+    app.use((0, http_proxy_middleware_1.createProxyMiddleware)({
+        target: 'http://localhost:3000',
+        // websocket support
+        ws: true,
+        // turn off all logs from proxy middleware
+        logLevel: 'silent',
+    }));
+    // 2. Handle active development of React application
+    // 3.
     // Asyncify server startup
     return new Promise((resolve, reject) => {
         app.listen(port, resolve).on('error', reject);
--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,8 +1,23 @@
 import express from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 export const serve = (port: number, filename: string, dir: string) => {
   // Initial setup for express server
   const app = express();
+  // 1. Load up react application inside browser
+  app.use(
+    createProxyMiddleware({
+      target: 'http://localhost:3000',
+      // websocket support
+      ws: true,
+      // turn off all logs from proxy middleware
+      logLevel: 'silent',
+    })
+  );
+
+  // 2. Handle active development of React application
+
+  // 3.
 
   // Asyncify server startup
   return new Promise<void>((resolve, reject) => {
